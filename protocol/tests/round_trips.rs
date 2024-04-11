21f46e73e63f94aa67bb9a9ff2deab0e3c286f92
Nick Johnson <nick@yonson.dev>
2024-04-11 12:13:52 -0700

    Broken tests

--- protocol/tests/round_trips.rs
+++ protocol/tests/round_trips.rs
@@ -1,26 +1,31 @@
-use bip324::{Handshake, Network};
+use bip324::{Handshake, Network, Role};
 
 #[test]
 fn hello_world_happy_path() {
-    // Alice starts a connection with Bob by making a pub/priv keypair and sending a message to Bob.
     let mut init_message = vec![0u8; 64];
-    let alice_handshake_init = Handshake::new(Network::Mainnet, None, &mut init_message).unwrap();
+    let mut init_handshake =
+        Handshake::new(Network::Mainnet, Role::Initiator, None, &mut init_message).unwrap();
 
-    // Bob parses Alice's message, generates his pub/priv key, and sends a message back.
-    let bob_handshake_response =
-        Handshake::new_from_request(Network::Mainnet, None, &init_message).unwrap();
-    let mut response_message = vec![0u8; 100];
-    let mut bob = bob_handshake_response
-        .finalize(None, &mut response_message)
+    let mut resp_message = vec![0u8; 100];
+    let mut resp_handshake =
+        Handshake::new(Network::Mainnet, Role::Responder, None, &mut resp_message).unwrap();
+
+    resp_handshake
+        .complete_materials(init_message.try_into().unwrap(), &mut resp_message[64..])
         .unwrap();
-
-    // Alice finishes her handshake by using her keys from earlier, and sending a final message to Bob.
     let mut init_finalize_message = vec![0u8; 36];
-    let mut alice = alice_handshake_init
-        .finalize(
-            Some(response_message[0..64].try_into().unwrap()),
+    init_handshake
+        .complete_materials(
+            resp_message[0..64].try_into().unwrap(),
             &mut init_finalize_message,
         )
+        .unwrap();
+
+    let mut alice = init_handshake
+        .authenticate_garbage_and_version(&resp_message[64..])
+        .unwrap();
+    let mut bob = resp_handshake
+        .authenticate_garbage_and_version(&init_finalize_message)
         .unwrap();
 
     // Alice and Bob can freely exchange encrypted messages using the packet handler returned by each handshake.
