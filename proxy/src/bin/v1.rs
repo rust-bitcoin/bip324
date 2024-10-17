07e02f91a8767d16cd01ee61d4bccf3ba2e4f4ee
Nick Johnson <nick@yonson.dev>
2024-10-17 13:23:11 -0700

    Add logger to proxy app to clean up the noise

--- proxy/src/bin/v1.rs
+++ proxy/src/bin/v1.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 
 use bip324_proxy::{V1ProtocolReader, V1ProtocolWriter};
 use bitcoin::Network;
+use log::{debug, error, info};
 use tokio::{
     net::{TcpListener, TcpStream},
     select,
@@ -16,7 +17,7 @@ configure_me::include_config!();
 async fn proxy_conn(client: TcpStream, network: Network) -> Result<(), bip324_proxy::Error> {
     let remote_ip = bip324_proxy::peek_addr(&client, network).await?;
 
-    println!("Initialing remote connection {}.", remote_ip);
+    info!("Initialing remote connection {}.", remote_ip);
     let remote = TcpStream::connect(remote_ip).await?;
 
     let (client_reader, client_writer) = client.into_split();
@@ -27,13 +28,13 @@ async fn proxy_conn(client: TcpStream, network: Network) -> Result<(), bip324_pr
     let mut v1_remote_reader = V1ProtocolReader::new(remote_reader);
     let mut v1_remote_writer = V1ProtocolWriter::new(network, remote_writer);
 
-    println!("Setting up proxy loop.");
+    info!("Setting up proxy loop.");
 
     loop {
         select! {
             result = v1_client_reader.read() => {
                 let msg = result?;
-                println!(
+                debug!(
                     "Read {} message from client, writing to remote.",
                     msg.command()
                 );
@@ -41,7 +42,7 @@ async fn proxy_conn(client: TcpStream, network: Network) -> Result<(), bip324_pr
             },
             result = v1_remote_reader.read() => {
                 let msg = result?;
-                println!(
+                debug!(
                     "Read {} message from remote, writing to client.",
                     msg.command()
                 );
@@ -53,13 +54,15 @@ async fn proxy_conn(client: TcpStream, network: Network) -> Result<(), bip324_pr
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
+
     let (config, _) = Config::including_optional_config_files::<&[&str]>(&[]).unwrap_or_exit();
     let network = Network::from_str(&config.network).expect("parse-able network");
 
     let proxy = TcpListener::bind((&*config.bind_host, config.bind_port))
         .await
         .expect("Failed to bind to proxy port.");
-    println!(
+    info!(
         "Listening for connections on {}:{}",
         config.bind_host, config.bind_port,
     );
@@ -72,10 +75,10 @@ async fn main() {
         tokio::spawn(async move {
             match proxy_conn(stream, network).await {
                 Ok(_) => {
-                    println!("Proxy establilshed.");
+                    info!("Proxy establilshed.");
                 }
                 Err(e) => {
-                    println!("Ended connection with error: {e}.");
+                    error!("Ended connection with error: {e}.");
                 }
             };
         });
